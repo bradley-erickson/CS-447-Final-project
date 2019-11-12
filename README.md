d6beeaf5f1d950c58abd51b5bb73a17774941ef3
Bradley Erickson <bbwe24@gmail.com>
2019-11-12 17:52:21 -0600

    inital

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS-447-Final-project
+ 
---
+++ classifier_model_creator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Matt Dill, Bradley Erickson
+@class: CS 447: Machine Learning
+@date: Fall 2019
+@title: Final Project: Car Classifier
+"""
+
+# imports
+import cv2
+import json
+import keras
+import numpy as py
+import tensorflow as tf
+
+## keras imports
