e4caffdcdd0e657805416c56435984d1897e6ddf
Bradley Erickson <bbwe24@gmail.com>
2019-11-13 08:20:09 -0600

    images are read in and resized properly

--- classifier_model_creator.py
+++ classifier_model_creator.py
@@ -18,42 +18,57 @@ import tensorflow as tf
 
 
 # methods
-## data pre and post process
+## data preprocess
 def get_list_from_csv(file):
     """ Import CSV to list form """
     
     with open(file, 'r') as f:
         reader = csv.reader(f)
         your_list = list(reader)
-        
     return your_list
-
-
-def save_model_to_json(model, name="model.json"):
-    """ save the model to a json document """
-    model.save_weights(name+'.h5')
-    model_json  = model.to_json()
-    with open(name, 'w') as f:
-        json.dump(model_json, f)
    
 
 def get_car_pic_matrix(file_name, crop_points, size):
     """
         input the car file name and output the car matrix properly resized
         file_name = "00001.jpg"
-        crop_points = [min_x, max_x, max_y, min_y]
+        crop_points = [min_x, min_y, max_x, max_y]
         size = [pixels_x, pixels_y]
     """
-    image = cv2.imread(file_name)
-    cropped = image[crop_points[3]:crop_points[2], crop_points[0]:crop_points[1]]
-    print (cropped.shape)
-    #image = cv2.resize(image, (size[0], size[1]))
-    return image
-     
-## 
     
-car = get_car_pic_matrix("./data/images/00001.jpg", [39, 116, 569, 375], [200, 250])
-print (car.shape)
-car = car[375:569, ]
-cv2.imshow('sample', car)
-cv2.waitKey(0)
+    image = cv2.imread(file_name)
+    image = image[crop_points[1]:crop_points[3], crop_points[0]:crop_points[2]]
+    image = cv2.resize(image, (size[0], size[1]))
+    return image
+
+
+## model initialization
+    
+
+## training process
+    
+
+## data postprocess
+def save_model_to_json(model, name="model.json"):
+    """ save the model to a json document """
+    
+    model.save_weights(name+'.h5')
+    model_json  = model.to_json()
+    with open(name, 'w') as f:
+        json.dump(model_json, f)
+        
+        
+# testing stuff
+## final variables
+DATA_DIR = "./data"
+IMAGE_DIR = DATA_DIR + "/images/"
+RESIZE = [200, 200]
+LIST_DATA = get_list_from_csv(DATA_DIR + "/crop_size.csv")
+
+## specific to first image for testing
+image_number = 1
+### image_data = [min_x, min_y, max_x, max_y, classification, image_name]
+image_data = LIST_DATA[image_number - 1]
+image_path = IMAGE_DIR + image_data[5]
+crop_points = [int(i) for i in image_data[:4]]
+car = get_car_pic_matrix(image_path, crop_points, RESIZE)
